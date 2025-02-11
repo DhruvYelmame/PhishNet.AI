c5e753f82e828cecb0cc74ab0e727f35c61fd257
Dhruv Yelmame <135983534+DhruvYelmame@users.noreply.github.com>
2025-02-11 19:43:09 +0530

    Add files via upload

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import GithubIcon from "../../assets/png/github.png";
+
+function Footer() {
+  return (
+    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+      <div className="items-center grid-flow-col">
+        <p>Amit's Major Project</p>
+      </div>
+      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a href="https://github.com/amitkroutthedev/phishing-url-detection-frontend">
+          <img src={GithubIcon} alt="Github" />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
